73ea8885d9dc25775efdb9771d046010a07a842e
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-05-23 14:12:32 +0500

    Writen on lecture

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 describe Answer do
+  it { should belong_to :question}
   it { should validate_presence_of :question_id}
   it { should validate_presence_of :body}
-  it { should belong_to :question}
+
+
+  let(:test_answer) { create :answer }
+
+  it 'test' do
+    #p test_answer
+    question = test_answer.question
+    #p question
+    #p question.answers
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,23 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+ENV['RAILS_ENV'] = 'test'
+
+require "bundler/setup"
+
+::Bundler.require(:default, :test)
+
+require "shoulda/matchers"
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
