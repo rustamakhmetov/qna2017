37f1eacf955a2e1aa1cd4090badfa5c766bbde8c
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-06-04 22:20:33 +0500

    Правка замечаний

--- spec/acceptance/questions/create_question_spec.rb
+++ spec/acceptance/questions/create_question_spec.rb
@@ -18,7 +18,22 @@ feature 'Create question', %q{
     fill_in 'Body', with: 'Body 1'
     click_on 'Create'
 
+    expect(page).to have_content 'Title 1'
+    expect(page).to have_content 'Body 1'
     expect(page).to have_content 'Your question successfully created.'
+  end
+
+  scenario 'Authenticate user ask invalid question' do
+    sign_in(user)
+
+    visit questions_path
+    click_on 'Ask question'
+
+    fill_in 'Title', with: ''
+    fill_in 'Body', with: 'Body 1'
+    click_on 'Create'
+
+    expect(page).to have_content 'Title can\'t be blank'
   end
 
   scenario 'Non-authenticate user ties ask question' do
