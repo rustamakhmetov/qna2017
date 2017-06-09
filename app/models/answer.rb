de384368031316c6eca67047b24ae4004041871f
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-06-09 23:48:58 +0500

    Added method 'accept' to model of answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,11 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+
+  def accept!
+    transaction do
+      question.answers.update_all(accept: false)
+      update!(accept: true)
+    end
+  end
 end
