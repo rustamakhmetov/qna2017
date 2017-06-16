b5e61ed90d8530dacda20ff9d4b69d7cdc7294a1
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-06-16 17:19:14 +0500

    Rollback for all migrate model Attachment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616063652) do
+ActiveRecord::Schema.define(version: 20170609115009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,16 +25,6 @@ ActiveRecord::Schema.define(version: 20170616063652) do
     t.index ["accept"], name: "index_answers_on_accept"
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
-  end
-
-  create_table "attachments", force: :cascade do |t|
-    t.string "file"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "attachmentable_id"
-    t.string "attachmentable_type"
-    t.index ["attachmentable_id"], name: "index_attachments_on_attachmentable_id"
-    t.index ["attachmentable_type"], name: "index_attachments_on_attachmentable_type"
   end
 
   create_table "questions", force: :cascade do |t|
