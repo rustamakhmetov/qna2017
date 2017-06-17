0525d86e7a228220afb8383a4cf95866a671539f
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-06-18 00:42:15 +0500

    In model Question added referense on model Attachment

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe Question do
   it { should belong_to :user}
   it { should have_many(:answers).dependent(:destroy)}
+  it { should have_many(:attachments).dependent(:destroy) }
   it { should validate_presence_of :title}
   it { should validate_presence_of :body}
+  it { should accept_nested_attributes_for :attachments }
 end
