92232a1e73b1c6b792038e9932dd51c31ad7d245
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-06-18 00:57:06 +0500

    User can added files to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,6 @@ class Question < ApplicationRecord
   has_many :attachments, as: :attachable, dependent: :destroy
 
   validates :title, :body, presence: true
-  
+
   accepts_nested_attributes_for :attachments
 end
