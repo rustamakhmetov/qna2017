2c2d13835f7758b13f5ed32094348c940f95fc69
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-06-19 11:47:25 +0500

    Refactoring: attachment render to partial

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,4 @@
 class Attachment < ApplicationRecord
-  delegate :identifier, to: :file
-  delegate :filename, to: :file
-
   belongs_to :attachable, polymorphic: true, optional: true
 
   mount_uploader :file, FileUploader
