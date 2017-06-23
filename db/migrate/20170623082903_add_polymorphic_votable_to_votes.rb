bdd6ff6e53ef413514c431b112ed91de5f81e1e6
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-06-23 13:42:41 +0500

    Create model Vote

---
+++ db/migrate/20170623082903_add_polymorphic_votable_to_votes.rb
@@ -0,0 +1,7 @@
+class AddPolymorphicVotableToVotes < ActiveRecord::Migration[5.1]
+  def change
+    add_column :votes, :votable_id, :integer
+    add_column :votes, :votable_type, :string
+    add_index :votes, [:votable_id, :votable_type]
+  end
+end
