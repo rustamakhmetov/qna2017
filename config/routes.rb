2351c340368f585f623ebe49ccde9883c3e642d3
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-06-23 23:03:49 +0500

    Add concern votable to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  concern :votable do
+    member do
+      patch 'vote_up'
+      patch 'vote_down'
+    end
+  end
+
   resources :questions do
     resources :answers, shallow: true, except: [:index, :show, :new, :edit] do
       patch 'accept', on: :member
     end
-    patch 'vote_up', on: :member
-    patch 'vote_down', on: :member
+    concerns :votable
   end
+
   resources :attachments, only: [:destroy]
+
   root 'questions#index'
 end
