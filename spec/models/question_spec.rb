9c38253ef6e1d56f4947c74d0ae4a166a20da69a
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-06-23 13:44:36 +0500

    Added votes to question model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,6 +4,7 @@ describe Question do
   it { should belong_to :user}
   it { should have_many(:answers).dependent(:destroy)}
   it { should have_many(:attachments).dependent(:destroy) }
+  it { should have_many(:votes).dependent(:destroy) }
   it { should validate_presence_of :title}
   it { should validate_presence_of :body}
   it { should accept_nested_attributes_for :attachments }
