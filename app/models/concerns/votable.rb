e24b033b879b91bb519c5fcb82ee8ea3a5582ed3
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-06-30 20:23:59 +0500

    Voting methods are moved to the voting model

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -3,4 +3,34 @@ module Votable
   included do
     has_many :votes, as: :votable, dependent: :destroy
   end
+
+  def vote_up!(user)
+    vote_reset!(user) if vote_down_exists?(user)
+    votes.create(user: user, value: 1) unless user&.author_of?(self) || vote_up_exists?(user)
+  end
+
+  def vote_down!(user)
+    vote_reset!(user) if vote_up_exists?(user)
+    votes.create(user: user, value: -1) unless user&.author_of?(self) || vote_down_exists?(user)
+  end
+
+  def vote_up_exists?(user)
+    votes.exists?(user_id: user.id, value: 1)
+  end
+
+  def vote_down_exists?(user)
+    votes.exists?(user_id: user.id, value: -1)
+  end
+
+  def vote_exists?(user)
+    votes.exists?(user_id: user.id)
+  end
+
+  def vote_reset!(user)
+    votes.where(user_id: user.id).destroy_all
+  end
+
+  def vote_rating
+    votes.sum(:value)
+  end
 end
--- spec/models/concerns/votable_spek.rb
+++ spec/models/concerns/votable_spek.rb
@@ -1,13 +1,84 @@
 require 'rails_helper'
 
 shared_examples_for "votable" do
-  let(:model) { described_class }
+  let!(:model) { described_class }
   let!(:user) { create(:user) }
-  let(:vote) { create(:vote, user: user) }
-  
-  it '#votes' do
-    question = create(model.to_s.underscore.to_sym)
-    question.votes << vote
-    expect(question.votes).to match_array(vote)
+
+  describe '#votes' do
+    let!(:object) { create(model.to_s.underscore.to_sym) }
+
+    it 'vote up' do
+      expect { object.vote_up!(user) }.to change(Vote, :count).by(1)
+    end
+
+    it 'vote down' do
+      expect { object.vote_down!(user) }.to change(Vote, :count).by(1)
+    end
+
+    it 'vote rating' do
+      object.vote_up!(user)
+      object.vote_down!(user)
+      expect(object.vote_rating).to eq -1
+    end
+
+    it 'vote up exists' do
+      object.vote_up!(user)
+      expect(object.vote_up_exists?(user)).to eq true
+      expect(object.vote_exists?(user)).to eq true
+    end
+
+    it 'vote down exists' do
+      object.vote_down!(user)
+      expect(object.vote_down_exists?(user)).to eq true
+      expect(object.vote_exists?(user)).to eq true
+    end
+
+    it 'vote reset' do
+      object.vote_up!(user)
+      object.vote_reset!(user)
+      expect(object.vote_rating).to eq 0
+    end
+
+    describe 'Author of question' do
+      it 'can not vote up for him' do
+        expect { object.vote_up!(object.user) }.to_not change(Vote, :count)
+      end
+
+      it 'can not vote down for him' do
+        expect { object.vote_down!(object.user) }.to_not change(Vote, :count)
+      end
+    end
+
+    describe "Authenticate user vote for object only one time" do
+      it "vote up" do
+        expect do
+          object.vote_up!(user)
+          object.vote_up!(user)
+        end.to change(Vote, :count).by(1)
+      end
+
+      it "vote down" do
+        expect do
+          object.vote_down!(user)
+          object.vote_down!(user)
+        end.to change(Vote, :count).by(1)
+      end
+
+      it 'vote down after vote up' do
+        object.vote_up!(user)
+        expect(object.vote_up_exists?(user)).to eq true
+        object.vote_down!(user)
+        expect(object.vote_up_exists?(user)).to eq false
+        expect(object.vote_down_exists?(user)).to eq true
+      end
+
+      it 'vote up after vote down' do
+        object.vote_down!(user)
+        expect(object.vote_down_exists?(user)).to eq true
+        object.vote_up!(user)
+        expect(object.vote_down_exists?(user)).to eq false
+        expect(object.vote_up_exists?(user)).to eq true
+      end
+    end
   end
 end
