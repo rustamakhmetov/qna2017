f8a8a670cd43adf7fba4f06f0a22b7b17cc42577
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-07-01 12:48:21 +0500

    Authenticate user vote for answer only one time

--- spec/acceptance/answers/vote_for_answer_spec.rb
+++ spec/acceptance/answers/vote_for_answer_spec.rb
@@ -35,6 +35,93 @@ feature 'Vote for answer', %q{
     end
   end
 
+  describe 'Authenticate user vote for answer only one time' do
+
+    scenario "vote up", js: true do
+      sign_in(user)
+
+      visit question_path(answer.question)
+
+      within("div#answer#{answer.id} > .vote") do
+        expect(page).to have_css ".vote-up"
+        within(".vote-count") do
+          expect(page).to have_content "0"
+        end
+        find(:css, ".vote-up").click
+        wait_for_ajax
+        find(:css, ".vote-up").click
+        wait_for_ajax
+
+        within(".vote-count") do
+          expect(page).to have_content "1"
+        end
+      end
+    end
+
+    scenario "vote down", js: true do
+      sign_in(user)
+
+      answer.votes << create(:vote, user: user, votable: answer, value: 1)
+      answer.votes << create(:vote, user: create(:user), votable: answer, value: 1)
+
+      visit question_path(answer.question)
+
+      within("div#answer#{answer.id} > .vote") do
+        expect(page).to have_css ".vote-down"
+        within(".vote-count") do
+          expect(page).to have_content "2"
+        end
+        find(:css, ".vote-down").click
+        wait_for_ajax
+        find(:css, ".vote-down").click
+        wait_for_ajax
+
+        within(".vote-count") do
+          expect(page).to have_content "0"
+        end
+      end
+    end
+
+    scenario "vote down after vote up", js: true do
+      sign_in(user)
+
+      visit question_path(answer.question)
+
+      within("div#answer#{answer.id} > .vote") do
+        within(".vote-count") do
+          expect(page).to have_content "0"
+        end
+        find(:css, ".vote-up").click
+        wait_for_ajax
+        find(:css, ".vote-down").click
+        wait_for_ajax
+
+        within(".vote-count") do
+          expect(page).to have_content "-1"
+        end
+      end
+    end
+
+    scenario "vote up after vote down", js: true do
+      sign_in(user)
+
+      visit question_path(answer.question)
+
+      within("div#answer#{answer.id} > .vote") do
+        within(".vote-count") do
+          expect(page).to have_content "0"
+        end
+        find(:css, ".vote-down").click
+        wait_for_ajax
+        find(:css, ".vote-up").click
+        wait_for_ajax
+
+        within(".vote-count") do
+          expect(page).to have_content "1"
+        end
+      end
+    end
+  end
 
   scenario 'Non-authenticate user ties vote for answer', js: true do
     visit question_path(answer.question)
