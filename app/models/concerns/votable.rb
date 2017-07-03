54fe67427c85c3c63671588765b8578faa1542bf
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-07-03 15:14:35 +0500

    Refactoring

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -5,25 +5,11 @@ module Votable
   end
 
   def vote_up!(user)
-    unless user&.author_of?(self)
-      if vote_up_exists?(user)
-        vote_reset!(user)
-      else
-        vote_reset!(user) if vote_down_exists?(user)
-        votes.create(user: user, value: 1)
-      end
-    end
+    vote_change(user, :up)
   end
 
   def vote_down!(user)
-    unless user&.author_of?(self)
-      if vote_down_exists?(user)
-        vote_reset!(user)
-      else
-        vote_reset!(user) if vote_up_exists?(user)
-        votes.create(user: user, value: -1)
-      end
-    end
+    vote_change(user, :down)
   end
 
   def vote_up_exists?(user)
@@ -50,7 +36,22 @@ module Votable
   private
 
   def reload_attribute(attr)
-    value = self.class.where(:id=>id).select(attr).first[attr]
+    value = self.class.where(id: id).select(attr).first[attr]
     self[attr] = value
+  end
+
+  def vote_change(user, act)
+    unless user&.author_of?(self)
+      vote_act = vote_action(user)
+      vote_reset!(user)
+      if vote_act != act
+        votes.create(user: user, value: (act == :up ? 1 : -1))
+      end
+    end
+  end
+
+  def vote_action(user)
+    vote = votes.where(user_id: user.id).first
+    (vote.value == 1 ? :up : :down) if vote
   end
 end
