3f85d7ebf603f6c25374c4751b7fdb1cd72c0598
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-07-11 10:02:43 +0500

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704111327) do
+ActiveRecord::Schema.define(version: 20170710105630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20170704111327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["attachable_id", "attachable_type"], name: "index_attachments_on_attachable_id_and_attachable_type"
+  end
+
+  create_table "authorizations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authorizations_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -89,6 +98,7 @@ ActiveRecord::Schema.define(version: 20170704111327) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
+  add_foreign_key "authorizations", "users"
   add_foreign_key "comments", "users"
   add_foreign_key "questions", "users"
   add_foreign_key "votes", "users"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should have_many(:questions).dependent(:destroy)}
   it { should have_many(:answers).dependent(:destroy)}
+  it { should have_many(:authorizations).dependent(:destroy)}
   it { should validate_presence_of :email}
   it { should validate_presence_of :password}
 
@@ -32,6 +33,71 @@ RSpec.describe User, type: :model do
 
       it 'fake model without field user_id' do
         expect(user).to_not be_author_of("")
+      end
+    end
+  end
+
+  describe ".find_for_oauth" do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: "facebook", uid: "123456")}
+
+    context "user already has authorization" do
+      it "returns the user" do
+        user.authorizations.create(provider: "facebook", uid: "123456")
+        expect(User.find_for_oauth(auth)).to eq user
+      end
+    end
+
+    context "user has not authorization" do
+      context "user already exists" do
+        let(:auth) { OmniAuth::AuthHash.new(provider: "facebook", uid: "123456", info: { email: user.email })}
+
+        it "does not create new user" do
+          expect { User.find_for_oauth(auth) }.to_not change(User, :count)
+        end
+
+        it "creates new record for authorization" do
+          expect { User.find_for_oauth(auth) }.to change(user.authorizations, :count).by(1)
+        end
+
+        it "creates authorization with provider and uid" do
+          user = User.find_for_oauth(auth)
+          authorization = user.authorizations.first
+
+          expect(authorization.provider).to eq auth.provider
+          expect(authorization.uid).to eq auth.uid
+        end
+
+        it "returns the user" do
+          expect(User.find_for_oauth(auth)).to eq user
+        end
+      end
+    end
+
+    context "user does not exists" do
+      let(:auth) { OmniAuth::AuthHash.new(provider: "facebook", uid: "123456", info: { email: "new@user.com" })}
+      it "creates new user" do
+        expect { User.find_for_oauth(auth) }.to change(User, :count).by(1)
+      end
+
+      it "returns new user" do
+        expect( User.find_for_oauth(auth) ).to be_a(User)
+      end
+
+      it "fills user email" do
+        user = User.find_for_oauth(auth)
+        expect(user.email).to eq auth.info.email
+      end
+
+      it "creates new record for authorization" do
+        expect { User.find_for_oauth(auth) }.to change(Authorization, :count).by(1)
+      end
+
+      it "creates authorization with provider and uid" do
+        authorization = User.find_for_oauth(auth).authorizations.first
+
+        expect(authorization.provider).to eq auth.provider
+        expect(authorization.uid).to eq auth.uid
       end
     end
   end
