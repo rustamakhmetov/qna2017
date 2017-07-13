ef71fb922b1792113d0e2418f9904e239859a996
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-07-14 00:24:11 +0500

    Removed class_eval

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,21 +1,10 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def self.providers_callback_for(provider)
-    class_eval %Q{
-      def #{provider}
-        @user = User.find_for_omniauth(request.env['omniauth.auth'])
-        if @user.persisted?
-          sign_in_and_redirect @user, event: :authentication
-          set_flash_message(:notice, :success, kind: "#{provider.capitalize}") if is_navigational_format?
-        else
-          session["devise.#{provider}_data"] = request.env['omniauth.auth']
-          redirect_to new_user_registration_url
-        end
-      end
-    }
+  def facebook
+    provider_callback(:facebook)
   end
 
-  Devise.omniauth_providers.each do |provider|
-    providers_callback_for(provider)
+  def twitter
+    provider_callback(:twitter)
   end
 
   def after_sign_in_path_for(resource)
@@ -29,5 +18,18 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def failure
     redirect_to root_path
+  end
+
+  private
+
+  def provider_callback(provider)
+    @user = User.find_for_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "#{provider.capitalize}") if is_navigational_format?
+    else
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
   end
 end
