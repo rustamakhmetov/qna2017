20a996c3e2cc339a76683e1d85791af81504ca0f
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-07-17 18:48:01 +0500

    Released api v1

---
+++ spec/api/v1/profiles_spec.rb
@@ -0,0 +1,97 @@
+require 'rails_helper'
+
+describe 'Profile API' do
+  describe 'GET /me' do
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/profiles/me', params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access_token is invalid' do
+        get '/api/v1/profiles/me', params: { format: :json, access_token: '1234' }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:me) { create(:user) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get '/api/v1/profiles/me', params: { format: :json, access_token: access_token.token }}
+
+      it 'returns 200 status' do
+        expect(response).to be_success
+      end
+
+      %w(id email created_at updated_at admin).each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
+        end
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "does not contain #{attr}" do
+          expect(response.body).to_not have_json_path(attr)
+        end
+      end
+    end
+  end
+
+  describe 'GET /list' do
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/profiles/list', params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access_token is invalid' do
+        get '/api/v1/profiles/list', params: { format: :json, access_token: '1234' }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let!(:me) { create(:user) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+      let!(:user1) { create(:user) }
+      let!(:user2) { create(:user) }
+
+      before { get '/api/v1/profiles/list', params: { format: :json, access_token: access_token.token }}
+
+      it 'returns 200 status' do
+        expect(response).to be_success
+      end
+
+      it 'list size is 2' do
+        expect(response.body).to have_json_size(2)
+      end
+
+      it 'me not exists' do
+        expect(response.body).to_not include_json(me.to_json)
+      end
+
+      it 'user1 exists' do
+        expect(response.body).to include_json(user1.to_json)
+      end
+
+      it 'user2 exists' do
+        expect(response.body).to include_json(user2.to_json)
+      end
+
+      %w(id email created_at updated_at admin).each do |attr|
+        it "contains #{attr}" do
+          result = JSON(response.body)[0].to_json
+          expect(result).to be_json_eql(user1.send(attr.to_sym).to_json).at_path(attr)
+        end
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "does not contain #{attr}" do
+          result = JSON(response.body)[0].to_json
+          expect(result).to_not have_json_path(attr)
+        end
+      end
+    end
+  end
+end
