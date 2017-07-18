d36c184ccf85303a2f7e0613a77316c0c8f43797
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-07-18 20:14:56 +0500

    Added api for question with attachments and comments

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -54,4 +54,63 @@ describe 'Questions API' do
       end
     end
   end
+
+  describe 'GET /show' do
+
+    let!(:question) { create(:question) }
+
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get "/api/v1/questions/#{question.id}", params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access_token is invalid' do
+        get "/api/v1/questions/#{question.id}", params: { format: :json, access_token: '1234' }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context "authorized" do
+      let!(:user) { create(:user) }
+      let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+      let!(:attachment) { create(:attachment, attachable: question) }
+      let!(:comments) { create_list(:comment, 2, commentable: question )}
+
+      before { get "/api/v1/questions/#{question.id}", params: { format: :json, access_token: access_token.token }}
+
+      it 'returns 200 status' do
+        expect(response).to be_success
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "question object contains #{attr}" do
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("#{attr}")
+        end
+      end
+
+      context 'attachments' do
+        it 'returns list' do
+          expect(response.body).to have_json_size(1).at_path("attachments")
+        end
+
+        it "contains url" do
+          expect(response.body).to be_json_eql(attachment.file.url.to_json).at_path("attachments/0/url")
+        end
+      end
+
+      context "comments" do
+        it 'returns list' do
+          expect(response.body).to have_json_size(2).at_path("comments")
+        end
+
+        %w(id body created_at updated_at).each do |attr|
+          it "object contains #{attr}" do
+            comment = comments.first
+            expect(response.body).to be_json_eql(comment.send(attr.to_sym).to_json).at_path("comments/0/#{attr}")
+          end
+        end
+      end
+    end
+  end
 end
