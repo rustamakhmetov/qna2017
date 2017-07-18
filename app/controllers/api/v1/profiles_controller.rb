42998f850b4cdb32b6a9fb5c96876b60693c1d08
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-07-18 09:53:55 +0500

    Added cancan authorize

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProfilesController < ApplicationController
   before_action :doorkeeper_authorize!
-  skip_authorization_check
+  authorize_resource class: false
 
   respond_to :json
 
@@ -16,5 +16,9 @@ class Api::V1::ProfilesController < ApplicationController
 
   def current_resource_owner
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id)
+  end
+
+  def current_user
+    current_resource_owner
   end
 end
