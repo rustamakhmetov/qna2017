630c99dd4c6eddcb0e256b5340660a56b9a9cb98
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-07-18 10:16:03 +0500

    Check for existence doorkeeper_token

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::ProfilesController < ApplicationController
   protected
 
   def current_resource_owner
-    @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id)
+    @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
 
   def current_user
