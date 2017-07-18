9484603bc0315d411fdb1917130332ead6840071
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-07-18 13:40:18 +0500

    Added short_title field for question's api

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -31,6 +31,10 @@ describe 'Questions API' do
         expect(response.body).to have_json_size(2)
       end
 
+      fit 'question object contains short_title' do
+        expect(response.body).to be_json_eql(question.title.truncate(10).to_json).at_path("0/short_title")
+      end
+
       %w(id title body created_at updated_at).each do |attr|
         it "question object contains #{attr}" do
           expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("0/#{attr}")
