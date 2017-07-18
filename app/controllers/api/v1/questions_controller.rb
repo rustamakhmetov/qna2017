b0df0810a0fd3a6500184d13e244f1eb4364ef4f
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-07-18 11:48:31 +0500

    Added api for questions

---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::QuestionsController < Api::V1::BaseController
+  authorize_resource(class: (controller_name.classify.constantize rescue nil).present?)
+
+  def index
+    @questions = Question.all
+    respond_with @questions.to_json(include: :answers)
+  end
+end
