20ea851ebe31839b73469e92f15e9d4541f7b946
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-07-19 13:32:31 +0500

    Refactoring

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BaseController < ApplicationController
   before_action :doorkeeper_authorize!
 
+  alias_attribute :current_user, :current_resource_owner
 
   respond_to :json
 
@@ -8,9 +9,5 @@ class Api::V1::BaseController < ApplicationController
 
   def current_resource_owner
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token&.resource_owner_id
-  end
-
-  def current_user
-    current_resource_owner
   end
 end
