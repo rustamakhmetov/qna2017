0c48f62cbb782b5b1db485c357f1fec0b29e15e8
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-08-07 13:08:27 +0500

    change action mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,9 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: '212.237.37.163' }
   config.x.active_job.default_url_options = { host: "212.237.37.163" }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 end
